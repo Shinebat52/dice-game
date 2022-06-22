78e8c6df3e4011e8cbd30f7faa1d6d26ee7e8afc
Shinee <gshinebat1@gmail.com>
2022-06-22 15:08:00 +0800

    Toglochuudiin active-iig solino mun odoogin onoog gargaj irne

--- app.js
+++ app.js
@@ -14,9 +14,24 @@ document.getElementById('current-1').textContent = '0'
 var diceDom = document.querySelector('.dice')
 
 diceDom.style.display = 'none'
+//shoog shideh event listener
 document.querySelector('.btn-roll').addEventListener('click', function () {
+  //randomoor 1-6 hurtel too gargah
   var diceNumber = Math.floor(Math.random() * 6) + 1
   diceDom.style.display = 'block'
   diceDom.src = 'dice-' + diceNumber + '.png'
   //alert('Shoo buulaa : ' + diceNumber)
+  if (diceNumber != 1) {
+    roundScore += diceNumber
+    document.getElementById('current-' + activePlayer).textContent = roundScore
+  } else {
+    roundScore = 0
+    document.getElementById('current-' + activePlayer).textContent = roundScore
+    activePlayer == 0 ? (activePlayer = 1) : (activePlayer = 0)
+    // if (activePlayer == 0) activePlayer = 1
+    // else activePlayer = 0
+    document.querySelector('.player-0-panel').classList.toggle('active')
+    document.querySelector('.player-1-panel').classList.toggle('active')
+    diceDom.style.display = 'none'
+  }
 })
