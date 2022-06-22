d9aa901ea0967c9541c7c813303bf51dd8aa6a55
Shinee <gshinebat1@gmail.com>
2022-06-22 16:17:36 +0800

    toglogchiin eeljiig solidog bolgono

--- app.js
+++ app.js
@@ -15,6 +15,20 @@ var diceDom = document.querySelector('.dice')
 
 diceDom.style.display = 'none'
 //shoog shideh event listener
+document.querySelector('.btn-hold').addEventListener('click', function () {
+  scores[activePlayer] += roundScore
+  document.getElementById('score-' + activePlayer).textContent =
+    scores[activePlayer]
+  if (scores[activePlayer] >= 10) {
+    document
+      .querySelector('.player-' + activePlayer + '-panel')
+      .classList.add('winner')
+    document.getElementById('name-' + activePlayer).textContent = 'WINNER!!!'
+    document
+      .querySelector('.player-' + activePlayer + '-panel')
+      .classList.remove('active')
+  } else ToglogchdiinEeljiigSolino()
+})
 document.querySelector('.btn-roll').addEventListener('click', function () {
   //randomoor 1-6 hurtel too gargah
   var diceNumber = Math.floor(Math.random() * 6) + 1
@@ -25,13 +39,17 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     roundScore += diceNumber
     document.getElementById('current-' + activePlayer).textContent = roundScore
   } else {
-    roundScore = 0
-    document.getElementById('current-' + activePlayer).textContent = roundScore
-    activePlayer == 0 ? (activePlayer = 1) : (activePlayer = 0)
-    // if (activePlayer == 0) activePlayer = 1
-    // else activePlayer = 0
-    document.querySelector('.player-0-panel').classList.toggle('active')
-    document.querySelector('.player-1-panel').classList.toggle('active')
-    diceDom.style.display = 'none'
+    ToglogchdiinEeljiigSolino()
   }
 })
+
+function ToglogchdiinEeljiigSolino() {
+  roundScore = 0
+  document.getElementById('current-' + activePlayer).textContent = roundScore
+  activePlayer == 0 ? (activePlayer = 1) : (activePlayer = 0)
+  // if (activePlayer == 0) activePlayer = 1
+  // else activePlayer = 0
+  document.querySelector('.player-0-panel').classList.toggle('active')
+  document.querySelector('.player-1-panel').classList.toggle('active')
+  diceDom.style.display = 'none'
+}
