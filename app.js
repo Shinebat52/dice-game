47bb3b6037dc31e2a04e52312cc237033c400270
Shinee <gshinebat1@gmail.com>
2022-06-22 12:29:21 +0800

    SHoonii buusan toogoor shoonii zurgiig haruuldag bolson

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 //тоглогчийн ээлжийг хадгалах хувьсагч
-var activePlayer = 0;
+var activePlayer = 0
 //тоглогчийн цуглуулсан оноог хадгалах хувьсагч
-var scores = [0, 0];
+var scores = [0, 0]
 //Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
-var roundScore = 0;
+var roundScore = 0
 //Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэг тоог энэ хувьсагчид хадгална.
-var dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector('#score-0').textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+document.getElementById('score-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+document.getElementById('current-1').textContent = '0'
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector('#current-1').textContent = 0;
+var diceDom = document.querySelector('.dice')
 
-document.querySelector(".dice").style.display = 'none';
-
-console.log(dice);
+diceDom.style.display = 'none'
+document.querySelector('.btn-roll').addEventListener('click', function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1
+  diceDom.style.display = 'block'
+  diceDom.src = 'dice-' + diceNumber + '.png'
+  //alert('Shoo buulaa : ' + diceNumber)
+})
